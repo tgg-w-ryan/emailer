510b5889c532ebe1e0ae3933b7763b36ee771cb1
tgg-w-ryan <williamhryan@gmail.com>
2016-01-25 17:48:05 -0600

    Basic framework (not working TT)

--- master.py
+++ master.py
@@ -5,4 +5,25 @@ Created on Mon Jan 25 17:40:30 2016
 @author: w_ryan
 """
 
-aasdf
+import smtplib
+
+
+print("Username")
+gm_username = input()
+print("Password")
+gm_pass = input()
+
+session = smtplib.SMTP('smtp.gmail.com', 587)
+session.ehlo()
+session.starttls()
+session.login(gm_username, gm_pass)
+
+headers = "\r\n".join(["from: " + gm_username,
+                       "subject: " + email_subject,
+                       "to: " + recipient,
+                       "mime-version: 1.0",
+                       "content-type: text/html"])
+
+# body_of_email can be plaintext or html!                    
+content = headers + "\r\n\r\n" + body_of_email
+session.sendmail(gm_username, recipient, content)
