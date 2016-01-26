816cafa2fc6f22051a226f4fdd03f172f539dc01
tgg-w-ryan <williamhryan@gmail.com>
2016-01-25 18:36:04 -0600

    Added example email link list, created email for sending messages

--- master.py
+++ master.py
@@ -7,16 +7,29 @@ Created on Mon Jan 25 17:40:30 2016
 
 import smtplib
 import getpass
+import os
+import docx
 
+
+
+
+
+
+# Script to send emails
 print("Username")
-gm_username = input()
+gm_username = "fundogfacts4you@gmail.com"
 print("Password")
-gm_pass = getpass.getpass(prompt='Password')
+gm_pass = "Freako312"
 
 session = smtplib.SMTP('smtp.gmail.com', 587)
 session.ehlo()
 session.starttls()
 session.login(gm_username, gm_pass)
+
+recipient = "ryan@tgggroup.com"
+email_subject = "FUCCASDJA"
+body_of_email = "some stuff"
+
 
 headers = "\r\n".join(["from: " + gm_username,
                        "subject: " + email_subject,
